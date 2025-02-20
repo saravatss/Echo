ff78b39eeea501316d0d0063d70e3c38973b5624
saravatss <149174589+saravatss@users.noreply.github.com>
2025-02-20 05:00:12 +0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,42 @@
 'use client'
 
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <Image
-        src="/next.svg"
-        alt="Next.js logo"
-        width={180}
-        height={38}
-        // fill
-        priority
-        unoptimized
+    <header className="headerSearch">
+      <div className="logoContainer">
+        <div className="upperLogo">
+          <img src="./burgericon.svg" className="burgerIcon" />
+          <img src="./Logo.svg" className="Logo" />
+        </div>
+        <nav className="navigation">
+          <li className="navItem navItem-active a">
+            Главная
+          </li>
+          <li className="navItem a">
+            Авторы
+          </li>
+          <li className="navItem a">
+            Карта
+          </li>
+        </nav>
+      </div>
+      <form className="searchContainer">
+        <img src="./searchicon.svg" className="searchIcon" />
+        <input
+          type="text"
+          id="searchInput"
+          className="searchInput"
+          placeholder="Введите запрос"
+          aria-label="Введите запрос"
+        />
+      </form>
+      <img
+        src="./user-icon.svg"
+        alt="User profile"
+        className="userIcon"
       />
-      <ul>
-        <li>
-          <Link href={`/blog`}>Blog</Link>
-        </li>
-      </ul>
     </header>
   );
 }
