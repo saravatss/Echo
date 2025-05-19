18fd0bce744c4e45dd6211d91725ef5a6682182b
saravatss <149174589+saravatss@users.noreply.github.com>
2025-05-19 05:19:31 +0300

    name of function

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import MapFeed from "@/components/Map";
 
-export default function Map() {
+export default function MapPage() {
   return (
     <div className="Map">
       <Header />
