bce2aa1336d58cd449f4b8ba530b3811be913700
saravatss <149174589+saravatss@users.noreply.github.com>
2025-06-27 08:04:39 +0300

    final

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script';
 import YMetrikaComponent from "@/external/YMetrica";
 import { Suspense } from 'react'
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Echo",
